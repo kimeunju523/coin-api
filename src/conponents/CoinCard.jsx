98d33918445fa6f7a26d605f724b611e6e4175f0
kimeunju <lov.ej.eg.ms@gmail.com>
2023-11-10 17:45:21 +0900

    last-commit

---
+++ src/conponents/CoinCard.jsx
@@ -0,0 +1,22 @@
+const CoinCard = ({ coinPrice, btcPrice }) => {
+  return (
+    <li className="flex-xl flex justify-center items-center">
+      <img
+        className="w-12 inline-block"
+        src={`./images/${coinPrice.market.substring(4)}.png`}
+        alt={`${coinPrice.market.substring(4)}`}
+      />
+      <span className="ml-2 w-16 inline-block">
+        {coinPrice.market.substring(4)}
+      </span>
+      ₩{" "}
+      <span className="ml-2 w-16 inline-block ">
+        {coinPrice.market.substring(0, 3) === "BTC"
+          ? (coinPrice.trade_price * btcPrice).toFixed(2).toLocaleString()
+          : coinPrice.trade_price.toLocaleString()}
+      </span>
+    </li>
+  );
+};
+
+export default CoinCard;
